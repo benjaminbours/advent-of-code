650610f4103b0767ab25b98d239029444ba53e65
Benjamin Bours <boursbenjamin@gmail.com>
2020-12-06 11:37:37 +0100

    day 6 part 2

--- day-6/src/main.rs
+++ day-6/src/main.rs
@@ -4,6 +4,55 @@ fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
     let lines = input.lines();
 
+    let mut group: Vec<&str> = Vec::new();
+    let lines_count = lines.clone().count();
+    let mut index = 0;
+    let groups: Vec<Vec<&str>> = lines.fold(Vec::new(), |mut acc, line| {
+        index += 1;
+
+        if index == lines_count {
+            group.push(line);
+            acc.push(group.clone());
+            return acc;
+        }
+
+        if line.is_empty() || index == lines_count {
+            acc.push(group.clone());
+            group = Vec::new();
+        } else {
+            group.push(line);
+        }
+
+        acc
+    });
+
+    let groups_sum: Vec<i32> = groups
+        .iter()
+        .map(|group| {
+            let first = group.get(0).unwrap();
+            let first_chars = first.chars();
+
+            println!("{:?}", group);
+
+            let res: Vec<char> = first_chars
+                .filter(|c| group.iter().all(|response| response.contains(*c)))
+                .collect();
+
+            res.len() as i32
+        })
+        .collect();
+
+    let result: i32 = groups_sum.iter().sum();
+
+    // println!("{:?}", groups);
+    println!("{:?}", groups_sum);
+    println!("{:?}", result);
+}
+
+fn part_1() {
+    let input = fs::read_to_string("input.txt").unwrap();
+    let lines = input.lines();
+
     // let mut group_index = 0;
     let mut group: Vec<&str> = Vec::new();
     let lines_count = lines.clone().count();
@@ -45,7 +94,7 @@ fn main() {
 
     let result: i32 = groups_sum.iter().sum();
 
-    // println!("{:?}", groups);
+    println!("{:?}", groups);
     println!("{:?}", groups_sum);
     println!("{:?}", result);
 }
