f89d8cf825220094f41b08d4727f2da57351b5ca
Benjamin Bours <boursbenjamin@gmail.com>
2020-12-09 20:56:00 +0100

    day 9 part 2

--- day-9/src/main.rs
+++ day-9/src/main.rs
@@ -8,10 +8,8 @@ fn main() {
 
     let numbers: Vec<i64> = lines.map(|line| line.parse::<i64>().unwrap()).collect();
 
-    // numbers.iter().for_each(||)
-
     let mut index = 0;
-    loop {
+    let invalid_number = loop {
         // do nothing the first 25 numbers
         if index <= 24 {
             index += 1;
@@ -28,15 +26,43 @@ fn main() {
                 if result == false {
                     println!("{}, previous: {:?}", n, previous_numbers);
                     println!("{}", result);
+                    break (Some(n.to_owned()));
                 }
             }
             None => {
-                break;
+                break (None);
             }
         }
 
         index += 1;
     }
+    .unwrap();
+
+    numbers.iter().enumerate().for_each(|(index_start, n)| {
+        let mut acc: Vec<&i64> = vec![];
+
+        let numbers_to_accumulate = &numbers[index_start..];
+
+        let mut index = 0;
+        let mut index_end = 0;
+        numbers_to_accumulate.iter().for_each(|x| {
+            acc.push(x);
+
+            let current_count: i64 = acc.clone().into_iter().sum();
+            if index > 1 && (current_count == invalid_number) {
+                index_end = index_start + index;
+                println!("HERE start: {}, end: {}", index_start, index_end);
+
+                let smallest = acc.iter().min().unwrap().clone();
+                let biggest = acc.iter().max().unwrap().clone();
+
+                println!("range: {:?}", acc);
+                println!("result: {}", smallest + biggest);
+            }
+
+            index += 1;
+        });
+    });
 }
 
 fn is_valid(sum: &i64, previous_numbers: &Vec<i64>) -> bool {
